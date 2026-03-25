aaa62805883da9981dd9e27e17e5a614e492235d
envy <@env>
2026-03-25 16:47:22 +0300

    fix server.py

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, Request
 import geoip2.database
 import uvicorn
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
@@ -8,7 +8,16 @@ app = FastAPI()
 def get_ip(request: Request):
     client_host = request.client.host
 
-    with geoip2.database.Reader('db/GeoLite2-Country.mmdb') as reader:
+    with geoip2.database.Reader("db/GeoLite2-Country.mmdb") as reader:
         response = reader.country(client_host)
-        print('recieved_report ', 'IP:', client_host, 'Country_Code:', response.country.iso_code, 'Country_name:', response.country.name)
+        print(
+            "report_recieved ",
+            "IP:",
+            client_host,
+            "Country_Code:",
+            response.country.iso_code,
+            "Country_name:",
+            response.country.name,
+        )
+
     return {"status": "OK"}
